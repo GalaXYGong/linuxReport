e027a6fd4bbd7cd805bad9664fd1460c6b54eac2
GalaXY <galaxygong@outlook.com>
2026-04-02 21:43:12 -0700

    filename

--- ubuntuReport.sh
+++ ubuntuReport.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# dpkg -l | awk 'NR>5 {print $2 "," $3}' > ubuntuReport.csv
-dpkg -l | awk 'BEGIN {print "["} NR>5 {printf "%s{\"package\":\"%s\",\"version\":\"%s\"}", (count++ ? "," : ""), $2, $3} END {print "\n]"}' > ubuntuReport.json
+# generate a report filename with the host_name, current date and time
+report_filename="$(hostname)_$(date +%Y%m%d_%H%M%S)"
+dpkg -l | awk 'NR>5 {print $2 "," $3}' > ${report_filename}.csv
+dpkg -l | awk 'BEGIN {print "["} NR>5 {printf "%s{\"package\":\"%s\",\"version\":\"%s\"}", (count++ ? "," : ""), $2, $3} END {print "\n]"}' > ${report_filename}.json
